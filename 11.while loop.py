c4927b9ed618d1bfdf47f9e17275362df8614139
Krishna mourya <maurya1389@gmail.com>
2024-12-27 20:26:04 +0530

    update

--- 11.while loop.py
+++ 11.while loop.py
@@ -2,6 +2,7 @@ i=1
 while(i<=10):
     print('number',i)
     i=i+1
+    
 
 i=1
 while(i<4):
--- any table.py
+++ any table.py
@@ -1,12 +1,18 @@
 #while loop
+n=int(input('enter any number : '))
+j=1
+while(j<n+1):
+    i=1
+    while(i<=10):
+        print(j,'*',i,'=',j*i)
+        i+=1
+    j+=1
+    print()
 
-i = 1
-while i <= 10:  
-    j = 1
-    while j <= 4:  
-        print(f"{i * j:4}", end="")  
-        j += 1
-    print() 
-    i+=1
+# for
 
-
+n=int(input('enter any number : '))
+for j in range (1,n+1):
+    for i in range(1,11):
+        print(j,'*',i,'=',j*i)
+        print()
